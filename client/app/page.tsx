bc967f2e46c2aacbad485f8a9b6c6221f29e01c1
Lucas Da Silva Junior <dasilvajunior881@gmail.com>
2025-02-11 20:23:30 +0100

    Added the contact section

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,16 @@
-
 export default function Home() {
   return (
     <section className=''>
       <div className='flex flex-col items-center justify-center h-screen'>
-        <h1 className='text-6xl font-bold'>Welcome to Spensify</h1>
-        <p className='text-xl'>The best budgeting app on the market</p>
+        <h1 className='text-xl md:text-6xl font-bold'>Welcome to Spensify</h1>
+        <p className='text-base md:text-xl'>The best budgeting app on the market</p>
+
+        <div className='mt-8 flex flex-col items-center'>
+          <h2 className='text-2xl font-semibold'>Ask questions and request support</h2>
+          <p className='text-lg'>We&apos;re here to help you manage your budget effectively!</p>
+          <a href='mailto:lucas.ds.developer@gmail.com' className='mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded'>Contact Us</a>
+        </div>
+
       </div>
     </section>
   );
